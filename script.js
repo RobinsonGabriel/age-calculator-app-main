0c21546f4a9fefbbbb49aaf1cb8ebe57fa0809b8
Gabriel Robinson <domogabriel@gmail.com>
2024-10-06 19:47:27 -0600

    Fixed minor bug where if cases wouldn't be checked on first key press (changed keypress to keyup). Added form validation for length and valid number range

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-var regex = /^[a-zA-Z_!@#$%^&*()\s][^$]*$/;
+var regex = /^[a-zA-Z_!@#$%^&*()\s]*$/; //Regex for invalid chars
+const months_30 = ["4", "6", "9", "11"];
+const months_31 = ["1", "3", "5", "7", "8", "10", "12"];
 
 $(document).ready(function () {
 	let year;
 	let month;
 	let day;
-	$("#year").keypress(() => {
+
+	//Below is a naive pass, will update after basic scaffolding is done
+	//TODO: refactor validation into single funciton (get each element with a focus event?)
+	//TODO: write case for empty input (NaN? whitespace??)
+	$("#year").keyup(() => {
 		year = $("#year").val();
-		if (year.match(regex)) {
+		if (!year.match(regex) || (Number(year) < 1900 && year.length === 4)) {
+			console.log(year);
 			$("#valid__year").css("visibility", "visible");
 		} else {
 			$("#valid__year").css("visibility", "hidden");
 		}
 	});
-	$("#month").keypress(() => {
+	$("#month").keyup(() => {
 		month = $("#month").val();
-		if (month.match(regex)) {
+		if (month.match(regex) || Number(month) > 12 || Number(month) < 1) {
 			$("#valid__month").css("visibility", "visible");
 		} else {
 			$("#valid__month").css("visibility", "hidden");
 		}
 	});
-	$("#day").keypress(() => {
-		day = $("#day").val();
-		if (day.match(regex)) {
+	$("#day").keyup(() => {
+		day = $("#day").val(); //TODO: catch edgecase for leap years, can I make this not this horrible? Which edge cases could I theoretically avoid using?
+		if (
+			(day.match(regex) || Number(day) > 31 || Number(day) < 1) &&
+			((!(month in months_30) && Number(day) >= 31) ||
+				(month === "2" && day >= 29))
+		) {
 			$("#valid__day").css("visibility", "visible");
 		} else {
 			$("#valid__day").css("visibility", "hidden");
@@ -33,3 +44,10 @@ $(document).ready(function () {
 function year_validate() {}
 function month_validate() {}
 function day_validate() {}
+
+function calc_diff() {
+	let currentDate = new Date();
+	let currentYear = currentDate.getFullYear();
+	let currentMonth = currentDate.getMonth() + 1;
+	let currentDay = currentDate.getDate();
+}
